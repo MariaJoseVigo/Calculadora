16e3abf9b913928016922b0369a0d399ad3022e9
MariaJoseVigo <131410272+MariaJoseVigo@users.noreply.github.com>
2023-04-23 16:16:40 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-let num1 = 10;
-let num2 = 2;
-let num3 = 3;
-if (num1>num2){ 
-    if ( num1>num3) {
-    console.log(num1)
+const CALCULAR = document.getElementById("calcular");
+const ERR = document.getElementById('error')
+
+CALCULAR.addEventListener("click", () => {
+    const INPUT = document.getElementById('peso')
+    const PESO = parseInt(INPUT.value)
+    let flujo;
+    let mantenimiento;
+    if (PESO > 0 && PESO <= 10) {
+        flujo = PESO * 100
+        mantenimiento = flujo * 1.5
+
+    } else if (PESO > 10 && PESO <= 20) {
+        flujo = 1000 + (PESO - 10) * 50
+        mantenimiento = flujo * 1.5
+
+
+    } else if (PESO > 20 && PESO <= 30) {
+        flujo = 1500 + (PESO - 20) * 20
+        mantenimiento = flujo * 1.5
+        console.log(flujo + "   " + mantenimiento)
+
+    } else if (PESO > 30) {
+        flujo = ((PESO * 4) + 7) / (PESO + 90)
+        mantenimiento = flujo * 1.5
     }
-} else if (num2>num3) {
-    console.log (num2)
-}
-else {
-    console.log (num3)
-}
+    console.log(flujo + "   " + mantenimiento)
+})
 
 
 
