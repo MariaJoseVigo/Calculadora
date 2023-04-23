9f43cc89c55201251b892c2d8796912bc35a463e
MariaJoseVigo <131410272+MariaJoseVigo@users.noreply.github.com>
2023-04-23 12:08:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let num1 = 10;
+let num2 = 2;
+let num3 = 3;
+if (num1>num2){ 
+    if ( num1>num3) {
+    console.log(num1)
+    }
+} else if (num2>num3) {
+    console.log (num2)
+}
+else {
+    console.log (num3)
+}
+
+
+
+
+
